133c4f59ea55dc32c6e11086c0eefaf9db3ae45f
Yashasvi <kmryashasvi@gmail.com>
2024-10-01 22:06:29 +0530

    modified debian and alacritty working

--- gitPush.cmd
+++ gitPush.cmd
@@ -1,9 +1,3 @@
-REM Updating alacritty config hosting
-curl https://raw.githubusercontent.com/yashlikescode/prepareLinux/refs/heads/main/alacritty.toml > ./public/alacritty
-
-REM Updating debian app installer hosting
-curl https://raw.githubusercontent.com/yashlikescode/prepareLinux/refs/heads/main/prepareDebian.sh > ./public/debian
-
 REM Adding all the files of this directory or its children and pushing them to remote server
 git add .
 @echo off
